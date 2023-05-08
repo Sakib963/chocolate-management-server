5ad95f2769b0016130ff2dcc7ece831552d1d026
Sakib963 <abdullah15-963@diu.edu.bd>
2023-05-09 03:16:29 +0600

    Added POST Route to create chocolate

--- index.js
+++ index.js
@@ -1,17 +1,56 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 const app = express();
-
+// Middleware
 app.use(cors());
+app.use(express.json());
 
 const port = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-    res.send("Chocolate Management Server is Running.")
-})
+app.get("/", (req, res) => {
+  res.send("Chocolate Management Server is Running.");
+});
 
+console.log(process.env.DB_USER, process.env.DB_PASS);
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.poiwoh3.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+// Creating MongoClient
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const database = client.db("chocolateDB");
+    const chocolateCollection = database.collection("chocolates");
+
+    app.get("/addChocolate", async (req, res) => {
+        const chocolate = req.body;
+
+        const result = await chocolateCollection.insertOne(chocolate);
+        res.send(result)
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+  }
+}
+run().catch(console.dir);
 
 app.listen(port, () => {
-    console.log(`Chocolate Management Server is Running on port: ${port}`);
-})
+  console.log(`Chocolate Management Server is Running on port: ${port}`);
+});
